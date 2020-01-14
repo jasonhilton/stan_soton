04e28483b8319129021fba9c9700b969ca91df77
Hilton J.D <jdh1d15@soton.ac.uk>
2020-01-14 17:44:48 +0000

    ncp plots

--- scripts/run_eight_schools.R
+++ scripts/run_eight_schools.R
@@ -10,4 +10,27 @@ schools_dat <- list(J = 8,
 # Example based on https://mc-stan.org/users/documentation/case-studies/divergences_and_bias.html
 
 
-# see  ?bayesplot::mcmc_nuts_divergence
+
+# Example based on https://mc-stan.org/users/documentation/case-studies/divergences_and_bias.html
+
+
+stan_out <- stan(file="stan/eight_schools.stan",
+                 data= schools_dat,
+                 iter=2000,
+                 cores=2,
+                 chains=2)
+
+
+
+np <- nuts_params(stan_out)
+lp <- log_posterior(stan_out)
+bayesplot::mcmc_nuts_divergence(np, lp)
+
+all_pars <- as.array(stan_out)
+
+bayesplot::mcmc_scatter(all_pars, pars = c("theta[1]", "tau"), np=np)
+
+bayesplot::mcmc_scatter(all_pars, pars = c("theta[1]", "tau"), 
+                        transformations=list(tau=log), np=np)
+
+
--- stan/eight_schools_non_centred.stan
+++ stan/eight_schools_non_centred.stan
@@ -7,13 +7,15 @@ data {
 parameters {
   real mu;
   real<lower=0> tau;
-  vector[J] theta;
+  vector[J] theta_tilde;
 }
 
 
 model {
+  vector[J] theta;
+  theta = mu + theta_tilde * tau;
   mu ~ normal(0, 5);
   tau ~ cauchy(0, 5);
-  theta ~ normal(mu,tau);
+  theta_tilde ~ normal(0,1);
   y ~ normal(theta, sigma);
 }
